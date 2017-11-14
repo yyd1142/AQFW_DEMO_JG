99c2dde11c7cf29490d7dca82ca6329fe854f496
YYD <yyd-yyd@hotmail.com>
2017-11-14 10:34:35 +0800

    api domain 命名规范化

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by enjoyzhou on 17-2-16.
  */
-import { httpGet, httpPost, httpPath, readFile } from './requestService'
+import {httpGet, httpPost, httpPath, readFile} from './requestService'
 import Conf from '../apiconf'
 
 export default {
@@ -13,10 +13,10 @@ export default {
     //登陆
     login: httpGet('/auth?m=login', Conf.loginDomain, true),
     //个人资料
-    getUserData: httpGet('/jg/user/info', Conf.personDomain),
-    getPersonInfo: httpGet('/jg/user/info', Conf.personDomain),
+    getUserData: httpGet('/jg/user/info', Conf.jgApiDomain),
+    getPersonInfo: httpGet('/jg/user/info', Conf.jgApiDomain),
     getUserInfo: httpGet('/person?m=info'),
-    postPerson: httpPost('/jg/user/add', Conf.personDomain), //添加或编辑 人员
+    postPerson: httpPost('/jg/user/add', Conf.jgApiDomain), //添加或编辑 人员
     getVersion: readFile('/version.json'),
     resetPasswrod: httpGet('/person'), //修改密码
     //通讯录
@@ -24,52 +24,52 @@ export default {
     getAddressList: httpGet('/list', Conf.directoriesDomain),
     getAddressInfo: httpGet('/info', Conf.directoriesDomain),
     //消息
-    getMsgList: httpGet('/message?m=list', Conf.msgDomain),
-    getMsgInfo: httpGet('/message?m=info', Conf.msgDomain),
-    hasNewMessage: httpGet('/message', Conf.msgDomain), // 判断首页小红点提示，是否有新消息
-    updateMessageStatus: httpGet('/message', Conf.msgDomain),
+    getMsgList: httpGet('/message?m=list', Conf.qyApiDomain),
+    getMsgInfo: httpGet('/message?m=info', Conf.qyApiDomain),
+    hasNewMessage: httpGet('/message', Conf.qyApiDomain), // 判断首页小红点提示，是否有新消息
+    updateMessageStatus: httpGet('/message', Conf.qyApiDomain),
     //新闻
     getNewsList: httpGet('/information/list', Conf.newsDomain),
     getNewsDetail: httpGet('/information/info', Conf.newsDomain),
     //通知公告
-    getNoticesList: httpGet('/dwNotice', Conf.noticeDomain),
+    getNoticesList: httpGet('/dwNotice', Conf.domain),
     getStatisticsList: httpGet('/messages/message/list', Conf.loginDomain),
-    getNoticesDetail: httpGet('/dwNotice', Conf.noticeDomain),
-    noticeIsRead: httpGet('/dwNotice', Conf.noticeDomain),
-    noticeReply: httpPost('/dwNotice?m=reply', Conf.noticeDomain),
-    getReplyInfo: httpGet('/dwNotice', Conf.noticeDomain),
-    getNoticeReplyList: httpGet('/dwNotice', Conf.noticeDomain),
+    getNoticesDetail: httpGet('/dwNotice', Conf.domain),
+    noticeIsRead: httpGet('/dwNotice', Conf.domain),
+    noticeReply: httpPost('/dwNotice?m=reply', Conf.domain),
+    getReplyInfo: httpGet('/dwNotice', Conf.domain),
+    getNoticeReplyList: httpGet('/dwNotice', Conf.domain),
     //风险管理
-    getYhkList: httpGet('/yhk?m=list', Conf.yhkDomain),
-    getYhkInfo: httpGet('/yhk?m=info', Conf.yhkDomain),
+    getYhkList: httpGet('/yhk?m=list', Conf.qyApiDomain),
+    getYhkInfo: httpGet('/yhk?m=info', Conf.qyApiDomain),
     addYhk: httpPost('/yhk?m=addByZF', Conf.domain),
-    updateYhk: httpPost('/yhk?m=update', Conf.yhkDomain),
+    updateYhk: httpPost('/yhk?m=update', Conf.qyApiDomain),
     //消防文件
     getXFSafetyFilesList: httpGet('/dwInfo/xfSafetyFile?m=info'),
     //考试
-    getExamsList: httpGet('/exam', Conf.examDomain),
-    startExam: httpGet('/exam', Conf.examDomain),
-    examResult: httpPost('/exam?m=examResult', Conf.examDomain),
-    getPastScoreList: httpGet('/exam', Conf.examDomain),
-    examScoreDetail: httpGet('/exam', Conf.examDomain),
+    getExamsList: httpGet('/exam', Conf.qyApiDomain),
+    startExam: httpGet('/exam', Conf.qyApiDomain),
+    examResult: httpPost('/exam?m=examResult', Conf.qyApiDomain),
+    getPastScoreList: httpGet('/exam', Conf.qyApiDomain),
+    examScoreDetail: httpGet('/exam', Conf.qyApiDomain),
     //安全评分
     getScoreList: httpGet('/dwScore?m=list', Conf.scoreDomain),
     getAllScoreList: httpGet('/dwScore?m=qydwScoreList', Conf.scoreDomain),
     getHistoryScoreList: httpGet('/dwScore', Conf.scoreDomain),
     getHistoryScoreDetail: httpGet('/dwScore', Conf.scoreDomain),
     //社会单位
-    getQyList: httpGet('/jg/user/jurisdiction', Conf.qyDomain),
+    getQyList: httpGet('/jg/user/jurisdiction', Conf.jgApiDomain),
     getQyInfo: httpGet('/dwInfo?m=info', Conf._domain),
-    getZRUnitList: httpGet('/dwInfo?m=info', Conf.msgDomain),
+    getZRUnitList: httpGet('/dwInfo?m=info', Conf.qyApiDomain),
     getZRUnitInfo: httpGet('/dwInfo/ZRDW?m=info'),
     getQyType: httpGet('/dwlx?m=dwlxList', Conf.platformDomain),  //获得所有企业类型
     addDWByZF: httpPost('/v2/dwInfo?m=addDWByZF', Conf._platformDomain), //添加 社会单位
-    getQyRecord: httpGet('/dwInfo?m=enforceLowList',Conf._pdomain), //获取执法记录或火灾记录
+    getQyRecord: httpGet('/dwInfo?m=enforceLowList', Conf._pdomain), //获取执法记录或火灾记录
     //服务单位
-    getFwList: httpGet('/fw/list', Conf.fwDomain),
-    getFwInfo: httpGet('/fw/info', Conf.fwDomain),
-    getCustomList: httpGet('/fw/signing', Conf.fwDomain),
-    getCustomInfo: httpGet('/jg/qy/info', Conf.fwDomain),
+    getFwList: httpGet('/fw/list', Conf.jgApiDomain),
+    getFwInfo: httpGet('/fw/info', Conf.jgApiDomain),
+    getCustomList: httpGet('/fw/signing', Conf.jgApiDomain),
+    getCustomInfo: httpGet('/jg/qy/info', Conf.jgApiDomain),
     //监管单位列表
     getJGList: httpGet('/dwInfo?m=jgBuMenList', Conf.domain),
     //建筑信息
@@ -77,9 +77,9 @@ export default {
     getJZDetail: httpGet('/jzInfo'),
     //图片资源
     getResourcesInfo: httpPath('/info/', Conf.resourcesDomain), //获取资源信息 列表
-    lowerDw: httpGet('/jg/lowerDw', Conf.fwDomain),
-    jgPersons: httpGet('/jg/user/list', Conf.fwDomain),
-    getPersonItem: httpGet('/jg/user/jurisdiction', Conf.fwDomain),
+    lowerDw: httpGet('/jg/lowerDw', Conf.jgApiDomain),
+    jgPersons: httpGet('/jg/user/list', Conf.jgApiDomain),
+    getPersonItem: httpGet('/jg/user/jurisdiction', Conf.jgApiDomain),
     getJGScoreTypes: httpGet('/dwScore', Conf.scoreDomain), //社会单位 分数等级模块
     getAppState: httpGet('/'), //获取APP状态
 
