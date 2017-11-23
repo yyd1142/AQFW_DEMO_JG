3c96055b98c3c296d1cf2b9eaa084c5a28bea7de
Queue <tantan313@126.com>
2017-11-24 00:56:36 +0800

    账号问题

--- src/views/DataCount/index.vue
+++ src/views/DataCount/index.vue
@@ -70,7 +70,7 @@
                     if (res && res.code == 0) {
                         let data = res.response;
                         let path = data.path.split('/');
-                        this.type = data.id == 1 ? 0 : 1;
+                        this.type = (data.id == 1 || path[1] == data.id) ? 0 : 1;
 //                        this.type = path[1] == data.id ? 0 : 1;
 //                        this.type = path[1] != data.id ? 0 : 1;
                         sessionStorage.setItem(`jgDwType${gId}`, this.type);
