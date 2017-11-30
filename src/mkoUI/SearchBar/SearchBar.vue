a5be38f9963d5f39c86ac16824e4c983c4add044
Queue <tantan313@126.com>
2017-11-30 11:26:01 +0800

    fix bug

--- src/mkoUI/SearchBar/SearchBar.vue
+++ src/mkoUI/SearchBar/SearchBar.vue
@@ -7,7 +7,7 @@
         <div class="box">
             <div class="sign-wrap" @click="focus">
                 <i class="icon"></i>
-                <span class="sign" v-show="!iptHasValue||!iptOnFocus">{{hintText || '搜索'}}</span>
+                <span class="sign" v-show="!iptHasValue&&!iptOnFocus">{{hintText || '搜索'}}</span>
             </div>
             <input ref="input" class="ipt" type="text" v-model="iptValue" @focus="inputOnFocus" @blur="inputOnBlur">
             <i class="mintui mintui-field-error cancel-xx" @click="inputReset" v-if="iptHasValue"></i>
