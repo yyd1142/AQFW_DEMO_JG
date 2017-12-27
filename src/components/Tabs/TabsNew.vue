05bea5a1adfd4d88fdb0f328c626d07869832e5c
Queue <tantan313@126.com>
2017-12-27 16:18:39 +0800

    样式调整

--- src/components/Tabs/TabsNew.vue
+++ src/components/Tabs/TabsNew.vue
@@ -45,6 +45,54 @@
                 font-size: 10px;
             }
         }
+        //icon
+        //首页底部tab
+        .icon-tabs(@x, @y) {
+            display: inline-block;
+            background: url('/static/icons/5_1/transparent_icons.png') @x @y no-repeat;
+            width: 30px;
+            height: 30px;
+            background-size: @bgSize;
+        }
+        .icon-tabs-home-0 {
+            .icon-tabs(-30px, -714px);
+        }
+
+        .icon-tabs-home-1 {
+            .icon-tabs(-30px, -744px);
+        }
+
+        .icon-tabs-task-0 {
+            .icon-tabs(-60px, -714px);
+        }
+
+        .icon-tabs-task-1 {
+            .icon-tabs(-60px, -744px);
+        }
+
+        .icon-tabs-dpm-0 {
+            .icon-tabs(-90px, -714px);
+        }
+
+        .icon-tabs-dpm-1 {
+            .icon-tabs(-90px, -744px);
+        }
+
+        .icon-tabs-staff-0 {
+            .icon-tabs(-120px, -714px);
+        }
+
+        .icon-tabs-staff-1 {
+            .icon-tabs(-120px, -744px);
+        }
+
+        .icon-tabs-my-0 {
+            .icon-tabs(-150px, -714px);
+        }
+
+        .icon-tabs-my-1 {
+            .icon-tabs(-150px, -744px);
+        }
     }
 </style>
 
